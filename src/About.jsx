f140847c69ddf40ffe0b37adda75158fe9f6e77c
soumyyaagrawal <soumyaagrawal2k6@gmail.com>
2026-01-27 10:19:41 +0530

    css module

---
+++ src/About.jsx
@@ -0,0 +1,17 @@
+import styles from './About.module.css';
+import './About.css';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className={styles.fontxl}>About Page</h1>
+      <h2 className={styles.primary}>About Page</h2>
+
+      <h1 className={`${styles.primary} ${styles.fontxl}`}>Content 1</h1>
+      <h1 className={`primary ${styles.fontxl}`}>Content 1</h1>
+      <h1 className={`secondary ${styles.fontxl}`}>Content 1</h1>
+    </div>
+  );
+};
+
+export default About;
