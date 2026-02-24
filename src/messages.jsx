def5003145fee68e93f9e287677c5656f79297dd
soumyyaagrawal <soumyaagrawal2k6@gmail.com>
2026-02-24 09:42:23 +0530

    added counter

--- src/messages.jsx
+++ src/messages.jsx
@@ -3,17 +3,24 @@ class Messages extends react.Component{
     constructor(props){
         super(props);
         this.state = {
-            name: "John"
+            name: "John",
+            counter:0
         }
     }
     changeName = () => {
         this.setState({name: "Jane"});
-    }   
+    }
+
+    counter = () => {
+        this.setState({counter: this.state.counter + 1});
+    }
     render(){
 
         return(
             <div>
                 <h1>hi, {this.state.name}</h1>
+                <h2>Counter: {this.state.counter}</h2>
+                <button onClick={()=>this.counter()}>Increment Counter</button>
                 <button onClick={()=>this.changeName()}>change name</button>
             </div>
         )
